4d06c93f63d6d1ce2a9376d39bc26e42ec3d607f
mrfleming <ericfleming8@gmail.com>
2018-03-24 16:40:20 -0500

    add a game to the league schedule

--- routes/schedules.js
+++ routes/schedules.js
@@ -1,5 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const redis = require('redis');
+
+let client  = redis.createClient();
+
+//Redis Client
+client.on('connect',function(){
+    console.log("Connected to redis... on schedule route");
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -10,4 +18,32 @@ router.get('/all', function(req, res, next) {
   res.render('leagueschedule');
 });
 
+router.get('/addgame', function(req, res, next) {
+  res.render('addgame');
+});
+
+router.post('/addgame', function(req, res, next) {
+    //grab what the user typed in
+    let gameid = 'game'+req.body.id;
+    let home = req.body.home;
+    let away = req.body.away;
+    let location = req.body.location;
+    let time = req.body.time;
+    //talk to the database
+    client.hmset(gameid, [
+        'home', home,
+        'away', away,
+        'location', location,
+        'time', time
+    ],function(err,reply){
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log(reply);
+            res.redirect('/schedules/all');
+        }
+    });
+
+});
 module.exports = router;
